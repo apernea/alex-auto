cf61a713a9a5635983dcf75e3a780770ee0241cf
apernea <apernea31@gmail.com>
2025-10-06 13:34:04 +0300

    Initial commit: Setup Spring Boot project with Java 24

---
+++ src/main/java/com/alexauto/AlexAutoApplication.java
@@ -0,0 +1,13 @@
+package com.alexauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexAutoApplication.class, args);
+    }
+
+}
