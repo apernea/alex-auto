5edbb65ac179a9fda1524ef46da633200bf5486d
apernea <apernea31@gmail.com>
2025-10-06 14:11:14 +0300

    Add CarService interface with CRUD operations

--- src/main/java/com/alexauto/service/CarService.java
+++ src/main/java/com/alexauto/service/CarService.java
@@ -1,5 +1,13 @@
 package com.alexauto.service;
 
+import java.util.List;
+
+import com.alexauto.model.Car;
+
 public interface CarService {
-    
+    List<Car> getAllCars();
+    Car getCarById(Long id);
+    Car createCar(Car car);
+    Car updateCar(Long id, Car car);
+    void deleteCar(Long id);
 }
