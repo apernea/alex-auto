7ee35c653cbbbd7dca0351d0e4354afa0229dd81
apernea <apernea31@gmail.com>
2025-10-15 19:37:39 +0300

    Added findByType() method

--- src/main/java/com/alexauto/repository/CarRepo.java
+++ src/main/java/com/alexauto/repository/CarRepo.java
@@ -1,10 +1,13 @@
 package com.alexauto.repository;
 
 import com.alexauto.model.Car;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepo extends JpaRepository<Car, Long> {
-
+    List<Car> findByType(String type);
 }
