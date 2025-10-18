8057427d3aae7439fd64839a2915df46796acc74
apernea <apernea31@gmail.com>
2025-10-18 15:50:16 +0300

    Updated Car model in order to include constraints and ainput data validation

--- src/main/java/com/alexauto/model/Car.java
+++ src/main/java/com/alexauto/model/Car.java
@@ -5,6 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -15,12 +19,28 @@ public class Car {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotBlank(message = "Make is mandatory")
+    @Size(max = 50, message = "Make cannot exceed 50 characters")
     private String make;
+
+    @NotBlank(message = "Model is mandatory")
+    @Size(max = 50, message = "Model cannot exceed 50 characters")
     private String model;
+
+    @Min(value = 1900, message = "Year must be 1900 or later")
     private int year;
+
+    @NotBlank(message = "Color is mandatory")
     private String color;
+
+    @Positive(message = "Price must be positive")
     private double price;
+
+    @NotBlank(message = "Description is mandatory")
     private String description;
+
+
     private String image_url;
+    @NotBlank(message = "Type is mandatory")
     private String type;
 }
