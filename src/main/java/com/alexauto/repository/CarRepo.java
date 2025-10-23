b4d45faf3045e7999cdef87ddb3832a244557b8a
apernea <apernea31@gmail.com>
2025-10-23 11:14:37 +0300

    Corrected CarRepo to work properly.

--- src/main/java/com/alexauto/repository/CarRepo.java
+++ src/main/java/com/alexauto/repository/CarRepo.java
@@ -1,13 +1,13 @@
 package com.alexauto.repository;
 
 import com.alexauto.model.Car;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepo extends JpaRepository<Car, Long> {
+public interface CarRepo extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
     List<Car> findByType(String type);
 }
