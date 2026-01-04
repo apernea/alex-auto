98489acdb4082750de456270351a36a1f13f5441
apernea <apernea31@gmail.com>
2026-01-04 16:38:34 +0200

    separated application setting into multiple yaml files for easier access

--- src/main/java/com/alexauto/repository/CarRepo.java
+++ src/main/java/com/alexauto/repository/CarRepo.java
@@ -3,11 +3,13 @@ package com.alexauto.repository;
 import com.alexauto.model.Car;
 import java.util.List;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Profile("db")
 public interface CarRepo extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
     List<Car> findByType(String type);
 }
