51e61a5212b93246190c15f6b42b9828d9108dac
apernea <apernea31@gmail.com>
2026-01-08 22:28:03 +0200

    added annotation Table to Car entity

--- src/main/java/com/alexauto/model/Car.java
+++ src/main/java/com/alexauto/model/Car.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -13,6 +14,7 @@ import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "cars")
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
