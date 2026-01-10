fbce5418ac05a6d54ae32c07b8effdb57f6bd6ba
apernea <apernea31@gmail.com>
2026-01-10 21:40:27 +0200

    added logging message

--- src/main/java/com/alexauto/AlexAutoApplication.java
+++ src/main/java/com/alexauto/AlexAutoApplication.java
@@ -1,13 +1,17 @@
 package com.alexauto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlexAutoApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(AlexAutoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AlexAutoApplication.class, args);
+        logger.info("Application started successfully.");
     }
-
 }
